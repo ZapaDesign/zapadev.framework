68a87520b8659cb29be34bea6d219c2ebcb57e67
zapa.poltava@gmail.com <aa|2NmTxM+72N3dD-jEh>
2022-02-08 14:43:10 +0200

    Add Class Router

--- public/index.php
+++ public/index.php
@@ -1,29 +1,29 @@
 <?php
 
-echo $query = rtrim($_SERVER['QUERY_STRING'], '/');
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+//echo $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
-Router::add('posts/add', [
-    'controller' => 'Posts',
-    'action'     => 'add'
-]);
+define('WWW', __DIR__);
+define('CORE', dirname(__DIR__) . '/vendor/core');
+define('ROOT', dirname(__DIR__));
+define('APP', dirname(__DIR__) . '/app');
 
-Router::add('posts', [
-    'controller' => 'Posts',
-    'action'     => 'index'
-]);
+spl_autoload_register(function ($class) {
+    $file = APP . "/controllers/$class.php";
+    if(is_file($file)) {
+        require_once $file;
+    }
+});
 
-Router::add('', [
-    'controller' => 'Main',
-    'action'     => 'index'
-]);
+Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'Main']);
+
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
+//Router::add('<controller:[a-z-]+>/<action:[a-z-]+>');
 
 debug(Router::getRoutes());
 
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-} else {
-    echo '404';
-}
+Router::dispatch($query);
